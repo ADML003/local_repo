dcd151e12c95bf61ed48f5d57d3bfd48d7bd7f12
ADML003 <study.admal@gmail.com>
2023-09-10 23:32:55 +0530

    some changes

--- mike.java
+++ mike.java
@@ -1,5 +1,6 @@
-public class mike{
+public class mike {
     public static void main(String[] args) {
-        System.out.println("Mike Ross is a brilliant attorney!! ");
+        System.out.println("Mike is back");
     }
+    
 }
