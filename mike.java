f37f3dc3e99fa964017aee05fd8b22c8c4dcf770
ADITYA MALHOTRA <110740007+ADML003@users.noreply.github.com>
2023-09-10 23:48:47 +0530

    Update mike.java

--- mike.java
+++ mike.java
@@ -1,6 +1,7 @@
 public class mike {
     public static void main(String[] args) {
         System.out.println("Mike is the next HARVEY SPECTER and the next senior partner");
+        System.out.println("new line");
     }
     
 }
